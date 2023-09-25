677fc46f1a4433b461e03e9dbb16aae2edc75e53
livroad <takaki0106kondo@icloud.com>
2023-09-25 22:44:30 +0900

    temp

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+from bs4 import BeautifulSoup
+from .models import ScrapedData
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-# Create your views here.
+def scrape_and_save(request):
+    url = 'https://www.rakuten-sec.co.jp/ITS/V_ACT_Login.html'  # スクレイピング対象のURLを指定
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # ページから必要な情報を抽出
+    data = soup.find('div', class_='target-element').text
+
+    # データベースに保存
+    scraped_data = ScrapedData(content=data)
+    scraped_data.save()
+
+    return HttpResponse(f'Successfully scraped and saved: {data}')
