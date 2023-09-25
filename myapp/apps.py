dddb03eee2294a2e91b094af79ba3f456ab56b48
livroad <takaki0106kondo@icloud.com>
2023-09-25 22:30:01 +0900

    change appname

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SampleappConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sampleapp'
+    name = 'myapp'
